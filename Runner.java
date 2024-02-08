f333871654fcb995f37733daf5e1fa5aa03d4f28
SebasVe <159404478+SebasVe@users.noreply.github.com>
2024-02-07 20:21:59 -0800

    Add files via upload

---
+++ Runner.java
@@ -0,0 +1,18 @@
+/*
+ * Activity 2.5.2
+ *
+ * The runner for the PhraseSolverGame
+ */
+public class Runner
+{
+  public static void main(String[] args) 
+  {
+    PhraseSolver p = new PhraseSolver(); 
+    p.play();
+    //Player yourName = new Player();
+    //loadPhrase test = new loadPhrase();
+
+    //System.out.println(yourName);
+  }
+
+} 
